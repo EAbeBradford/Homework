1c1561d36e382a8f43f14c3b4825fd21d2da104d
Mx. Bradford <emma_bradford@milton.com>
2024-11-15 08:19:32 -0500

    0i9tjun

--- src/MadLib.java
+++ src/MadLib.java
@@ -1,12 +1,6 @@
 public class MadLib {
 
 
-    public static void main(String[] args) {
-        new MadLib();
-    }
 
-    public MadLib (){
-        System.out.println("hello world");
 
-    }
 }
